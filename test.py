dfb73e888dbafe9365e654942338181f3f457d27
pcpandeyin <pcpandeyin@gmail.com>
2025-12-19 11:01:58 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+#finding grade of the student
+marks = int( input("enter marks "))
+grade =""
+if marks >= 33 and marks <45 :
+    grade ="D"
+elif marks >= 45 and marks <60:
+    grade ="C"
+elif marks >= 60 and marks <75:
+    grade ="B" 
+elif marks >=75:
+    grade ="A"
+else:
+    grade="F"
+
+print (" your marks achieved :",marks)
+print("your grade is :",grade)
+#over
+
